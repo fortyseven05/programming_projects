ec2daaa9869e8ed619b3289fc9f1dffd6a0dfa1c
Kyle <fortyseven05@gmail.com>
2018-08-22 19:29:27 -0400

    Added console message to end

--- CPL/Program.cs
+++ CPL/Program.cs
@@ -64,9 +64,9 @@ namespace CPL
 
             if (start < end)
             {
-                Console.WriteLine("First recursive call start: {0}, end: {1}", start, end);
+                Console.WriteLine("First recursive call start: {0}, end: {1}, pIndex: {2}", start, end, pIndex);
                 Partition(ref arr, start, pIndex - 1);
-                Console.WriteLine("Second recursive call start: {0}, end: {1}", start, end);
+                Console.WriteLine("Second recursive call start: {0}, end: {1}. pIndex: {2}", start, end, pIndex);
                 Partition(ref arr, pIndex + 1, end);
             }
 
@@ -89,3 +89,84 @@ namespace CPL
 
     }
 }
+
+/*
+ * Start QuickSort
+
+Start Partition
+
+Start of partition: start = 0, end = 5, pIndex = 0, pivot = 9
+Inside the for loop, swap call, i = 0, pIndex = 0
+Start Swap
+
+3 3
+3 1 4 1 5 9
+Inside the for loop, swap call, i = 1, pIndex = 1
+Start Swap
+
+1 1
+3 1 4 1 5 9
+Inside the for loop, swap call, i = 2, pIndex = 2
+Start Swap
+
+4 4
+3 1 4 1 5 9
+Inside the for loop, swap call, i = 3, pIndex = 3
+Start Swap
+
+1 1
+3 1 4 1 5 9
+Inside the for loop, swap call, i = 4, pIndex = 4
+Start Swap
+
+5 5
+3 1 4 1 5 9
+Values before Swap Call: end = 5, pIndex = 5
+Start Swap
+
+9 9
+Count: 1
+pIndex = 5
+After 2 partition
+3 1 4 1 5 9
+First recursive call start: 0, end: 5, pIndex: 5
+Start Partition
+
+Start of partition: start = 0, end = 4, pIndex = 0, pivot = 5
+Inside the for loop, swap call, i = 0, pIndex = 0
+Start Swap
+
+3 3
+3 1 4 1 5
+Inside the for loop, swap call, i = 1, pIndex = 1
+Start Swap
+
+1 1
+3 1 4 1 5
+Inside the for loop, swap call, i = 2, pIndex = 2
+Start Swap
+
+4 4
+3 1 4 1 5
+Inside the for loop, swap call, i = 3, pIndex = 3
+Start Swap
+
+1 1
+3 1 4 1 5
+Values before Swap Call: end = 4, pIndex = 4
+Start Swap
+
+5 5
+Count: 2
+Second recursive call start: 0, end: 5. pIndex: 5
+Start Partition
+
+Start of partition: start = 6, end = 5, pIndex = 6, pivot = 9
+Values before Swap Call: end = 5, pIndex = 6
+
+Unhandled Exception: System.IndexOutOfRangeException: Index was outside the bounds of the array.
+   at CPL.Program.Partition(Int32[]& arr, Int32 start, Int32 end) in C:\Users\fortyseven05\source\repos\CPL\CPL\Program.cs:line 35
+   at CPL.Program.QuickSort(Int32[]& arr, Int32 start, Int32 end) in C:\Users\fortyseven05\source\repos\CPL\CPL\Program.cs:line 70
+   at CPL.Program.Main(String[] args) in C:\Users\fortyseven05\source\repos\CPL\CPL\Program.cs:line 79
+Press any key to continue . . .
+*/
